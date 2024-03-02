8e980f4faa3883fdd9e9dd0b0a721ba267c352a9
basilry <basilry@gmail.com>
2024-03-02 13:44:01 +0900

    [update] 설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/",
+    basePath: "/kbslBlog",
+    assetPrefix: "/kbslBlog/",
 }
 
 module.exports = nextConfig
