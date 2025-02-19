30118c7a337ce2951b55f6d6e148a0796d770ef9
basilry <basilry@gmail.com>
2025-02-19 14:13:18 +0900

    [update] pm2 설정 완료

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,10 @@ module.exports = {
     apps: [
         {
             name: "kbslBlog_FE",
-            script: "./node_modules/.bin/next",
-            args: "next start -p 3000",
+            script: "./node_modules/next/dist/bin/next",
+            exec_mode: "fork_mode",
             instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: "300M",
-            env: {
-                NODE_ENV: "production",
-            },
+            watch: "true",
         },
     ],
 }
