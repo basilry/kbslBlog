1cfd57f40faec9ca53e526ad2233d326b7e8b829
basilry <basilry@gmail.com>
2025-12-08 18:04:15 +0900

    [update] github calander 버전 정리

--- src/app/Landing.tsx
+++ src/app/Landing.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { forwardRef, ReactElement, useEffect, useRef, useState } from "react"
-import GitHubCalendar, { Activity } from "react-github-calendar"
+import { GitHubCalendar, Activity } from "react-github-calendar"
 import { motion } from "framer-motion"
 import classNames from "classnames"
 import TextBasic from "@components/atom/TextBasic"
